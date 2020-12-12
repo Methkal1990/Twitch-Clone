3adde73377568799a2eabf1daec5e061b1558d26
Methkal <methkal.khalawi@gmail.com>
2020-12-12 23:58:30 +0100

    StreamShow implementation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
@@ -10,15 +10,17 @@ import history from '../history';
 
 const App = () => {
   return (
-    <div className="ui container">
+    <div className='ui container'>
       <Router history={history}>
         <div>
           <Header />
-          <Route exact path="/" component={StreamList} />
-          <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit/:id" component={StreamEdit} />
-          <Route path="/streams/delete/:id" component={StreamDelete} />
-          <Route path="/streams/show" component={StreamShow} />
+          <Switch>
+            <Route exact path='/' component={StreamList} />
+            <Route path='/streams/new' component={StreamCreate} />
+            <Route path='/streams/edit/:id' component={StreamEdit} />
+            <Route path='/streams/delete/:id' component={StreamDelete} />
+            <Route path='/streams/:id' component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -12,12 +12,15 @@ class StreamList extends React.Component {
   renderAdmin(stream) {
     if (stream.userId === this.props.currentUserId) {
       return (
-        <div className="right floated content">
-          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+        <div className='right floated content'>
+          <Link to={`/streams/edit/${stream.id}`} className='ui button primary'>
             Edit
           </Link>
-          <Link to={`/streams/delete/${stream.id}`} className="ui button negative">
-          Delete
+          <Link
+            to={`/streams/delete/${stream.id}`}
+            className='ui button negative'
+          >
+            Delete
           </Link>
         </div>
       );
@@ -27,12 +30,14 @@ class StreamList extends React.Component {
   renderList() {
     return this.props.streams.map((stream) => {
       return (
-        <div className="item" key={stream.id}>
+        <div className='item' key={stream.id}>
           {this.renderAdmin(stream)}
-          <i className="large middle aligned icon camera" />
-          <div className="content">
-            {stream.title}
-            <div className="description">{stream.description}</div>
+          <i className='large middle aligned icon camera' />
+          <div className='content'>
+            <Link to={`/streams/${stream.id}`} className='header'>
+              {stream.title}
+            </Link>
+            <div className='description'>{stream.description}</div>
           </div>
         </div>
       );
@@ -43,7 +48,7 @@ class StreamList extends React.Component {
     if (this.props.isSignedIn) {
       return (
         <div style={{ textAlign: 'right' }}>
-          <Link to="/streams/new" className="ui button primary">
+          <Link to='/streams/new' className='ui button primary'>
             Create Stream
           </Link>
         </div>
@@ -55,7 +60,7 @@ class StreamList extends React.Component {
     return (
       <div>
         <h2>Streams</h2>
-        <div className="ui celled list">{this.renderList()}</div>
+        <div className='ui celled list'>{this.renderList()}</div>
         {this.renderCreate()}
       </div>
     );
